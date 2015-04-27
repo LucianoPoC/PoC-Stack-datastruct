5243931e38e33f8d4548d5686723a2ce14cbaf38
Luciano Jr <luciano@lucianojr.com.br>
2015-04-27 19:39:11 -0300

    Change name, is Stack

---
+++ src/com/company/Stack.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Stack {
+    public void push(int value);
+    public int pop();
+    public int top();
+    public boolean empty();
+    public void release();
+}
---
+++ src/com/company/StackList.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class StackList implements Stack {
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int top() {
+        return 0;
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+
+    @Override
+    public void release() {
+
+    }
+}
